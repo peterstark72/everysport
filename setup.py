ed784b586b44d0cd6454e4f1bcaa826a01a50330
Peter Stark <peter.stark@menmo.se>
2013-07-02 20:04:38 +0200

    Editorial changes

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
-setup(name='everysport-py',
+setup(name='everysport',
       version='0.1',
       packages=['everysport'],
       description='A Python wrapper for the Everysport API',
       author='Peter Stark',
       author_email='peterstark72@gmail.com',
-      url='https://github.com/peterstark72/everysport-py',
+      url='https://github.com/peterstark72/everysport',
       scripts=['everysport.py']
       )
