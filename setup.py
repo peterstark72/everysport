d7b633370165afa0a6e3e8e425c362ff5fc618e9
peterstark72 <peterstark72@gmail.com>
2013-08-31 14:51:16 +0200

    version 1.2.1

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-
 from distutils.core import setup
-from io import open
-
-long_description = open(os.path.join(os.path.dirname(__file__), "README.md"), encoding='utf-8').read()
 
 setup(
       name='everysport',
-      version='1.2.0',
+      version='1.2.1',
       packages=['everysport'],
       description='A Python wrapper for the Everysport API',
       author='Peter Stark',
       author_email='peterstark72@gmail.com',
       url='https://github.com/peterstark72/everysport',
       keywords = "python everysport API sport",
-      long_description = long_description,
       classifiers=[
       	  'Development Status :: 4 - Beta',
           'Environment :: Console',
